624a3963b6536ea9da3577070f4860a417dbaddb
TTT <1794230131@qq.com>
2024-12-02 20:45:43 +0800

    分页查询

---
+++ src/main/java/com/itheima/mp/confi/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package com.itheima.mp.confi;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //1. 创建分页查询
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        paginationInnerInterceptor.setMaxLimit(1000L);
+
+        //2. 添加分页插件
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+
+        return interceptor;
+    }
+
+}
--- src/test/java/com/itheima/mp/service/IUserServiceTest.java
+++ src/test/java/com/itheima/mp/service/IUserServiceTest.java
@@ -1,5 +1,7 @@
 package com.itheima.mp.service;
 
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.mp.domain.po.User;
 import com.itheima.mp.domain.po.UserInfo;
 import org.junit.jupiter.api.Test;
@@ -35,6 +37,28 @@ class IUserServiceTest {
     @Test
     void testQuery(){
         List<User> users = userService.listByIds(List.of(1L, 2L, 4L));
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void testPageQuery(){
+        int pageNo = 1, pageSize = 2;
+        //1. 准备分页条件
+        //1.1. 分页条件
+        Page<User> page = Page.of(pageNo, pageSize);
+        //1.2.排序条件
+        page.addOrder(new OrderItem("balance", true));
+        page.addOrder(new OrderItem("id", true));
+
+        //2. 分页查询
+        Page<User> p = userService.page(page);
+
+        //3. 解析
+        long total = p.getTotal();
+        System.out.println("total =" + total);
+        long pages = p.getPages();
+        System.out.println("pages =" + pages);
+        List<User> users = p.getRecords();
         users.forEach(System.out::println);
     }
 
